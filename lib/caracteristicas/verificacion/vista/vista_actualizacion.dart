39524d1ce474cc0f4bc1769afbf4394634c2109f
Elquetud <jesusromeroarmenta@outlook.com>
2022-10-19 23:49:07 -0600

    se crea la vista de actualizacion y boton para regresar

--- lib/caracteristicas/verificacion/vista/vista_actualizacion.dart
+++ lib/caracteristicas/verificacion/vista/vista_actualizacion.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:board_game/caracteristicas/bloc.dart';
 
 class VistaMostrandoSolicitudActualizacion extends StatelessWidget {
+  const VistaMostrandoSolicitudActualizacion({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center();
+    final elBloc = context.read<BlocVerificacion>();
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            'Actualizate Compadre',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          TextButton(
+            onPressed: () {
+              elBloc.add(Creado());
+            },
+            child: Text(
+              'Volver',
+              style: Theme.of(context).textTheme.subtitle2,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
