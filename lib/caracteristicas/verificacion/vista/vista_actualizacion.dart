9d9b31793b4d640faaf359a5d77ddc3feb73e21a
Elquetud <jesusromeroarmenta@outlook.com>
2022-10-19 23:46:58 -0600

    se crea vista de actualizacion de usuario

---
+++ lib/caracteristicas/verificacion/vista/vista_actualizacion.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class VistaMostrandoSolicitudActualizacion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
