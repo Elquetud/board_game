85a44c8b8fbab9921985b5fb5fc9482e80bda7b2
Elquetud <jesusromeroarmenta@outlook.com>
2022-10-19 23:43:52 -0600

    se crea nombre_no_confirmado y se establece la vista y el mensaje de advertencia

--- lib/caracteristicas/verificacion/vista/nombre_no_confirmado.dart
+++ lib/caracteristicas/verificacion/vista/nombre_no_confirmado.dart
@@ -4,9 +4,21 @@ import 'package:board_game/caracteristicas/dominio/nick_formado.dart';
 import 'package:flutter/material.dart';
 
 class VistaMostrandoNombreNoConfirmado extends StatelessWidget {
+  final NickFormado nick;
+  const VistaMostrandoNombreNoConfirmado(this.nick, {super.key});
+
   @override
   Widget build(BuildContext context) {
     final elBloc = context.read<BlocVerificacion>();
-    return Center();
+    return Center(
+      child: Column(children: [
+        Text('este chavalon ${nick.valor} no esta en boardgamepeek'),
+        TextButton(
+            onPressed: () {
+              elBloc.add(Creado());
+            },
+            child: const Text('Volver'))
+      ]),
+    );
   }
 }
